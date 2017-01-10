0efed1c0529297caa9dfed2e29f4c57097aa2bff
prashant <prashantzhcet@gmail.com>
2017-01-10 14:06:38 +0400

    solution of pascal triangle

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demo-website
+# demo-website
---
+++ pascal_triangle_sol.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+
+    int n,i,j,ans=0,a;
+    scanf("%d",&n);
+    printf("1\n");
+    for(i=1;i<n;i++)
+    {
+    	for(j=0;j<=i;j++)
+    	{
+    		if(j==0)
+    			ans=1;
+    		else
+    		{
+    			i+j==n+1;
+    			a=i-j+1;
+    			ans=ans*a;
+    			ans=ans/j;
+			}
+    		printf("%ld ",ans);
+    	}
+    	printf("\n");
+    }
+    return 0;
+}
+
